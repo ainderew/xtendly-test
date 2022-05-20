7838a07d0687d4ef8ffed41ab80381109aa4557d
Andrew Pinon <andrewapinon@gmail.com>
2022-05-20 22:35:17 +0800

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: "#8bc540",
+        textColor: "#6D6E70",
+        half: "linear-gradient(89.92deg, #FFFFFF -0.67%, rgba(255, 255, 255, 0) 101.64%)"
+      },
+    },
+  },
+  plugins: [],
+};
